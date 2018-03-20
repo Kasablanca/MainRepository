dba228bad867b761c9ba5c4000565a1cd008c41a
Kasablanca <programonkey@sina.com>
2018-03-20 20:09:21 +0800

    clearn up

--- spring-boot-demo/src/main/java/com/lee/dao/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lee.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lee.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
