c4f827245c30a82ce0791d8824816dbe30c47b36
MIN.LEE <programonkey@sina.com>
2018-03-21 09:02:54 +0800

    overwrite local repository!

--- gradle-git-demo/src/main/java/com/min/Application.java
+++
@@ -1,9 +0,0 @@
-package com.min;
-
-public class Application {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- springboot-data/src/test/java/com/min/test/MainTest.java
+++
@@ -1,9 +0,0 @@
-package com.min.test;
-
-public class MainTest {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
