009e3cf53523b374c244b0394544682d4975e504
MIN.LEE <programonkey@sina.com>
2018-04-10 16:47:43 +0800

    @@

--- springboot-gradle/src/main/java/com/min/web/WebConfig.java
+++ springboot-gradle/src/main/java/com/min/web/WebConfig.java
@@ -1,5 +1,6 @@
 package com.min.web;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -9,14 +10,12 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module.Feature;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -33,14 +32,24 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 	
 	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {					
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		List<HttpMessageConverter<?>> forRemove = new ArrayList<>();
+		
+		for(HttpMessageConverter<?> converter:converters) {
+			if(converter instanceof MappingJackson2HttpMessageConverter) {
+				forRemove.add(converter);
+			}
+		}
+		
+		converters.removeAll(forRemove);
+		
 		Hibernate5Module module = new Hibernate5Module();
 		module.configure(Feature.FORCE_LAZY_LOADING, true);
 		
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(module);
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(module);
 		
-		converters.add(new MappingJackson2HttpMessageConverter(mapper));
+		converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
 	}
 	
 }
---
+++ springboot-gradle/src/test/java/com/min/TheMain.java
@@ -0,0 +1,9 @@
+package com.min;
+
+public class TheMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
