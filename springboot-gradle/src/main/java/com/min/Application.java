4c289063136d3a1ce4cfe9d27ee6509443292940
MIN.LEE <programonkey@sina.com>
2018-04-10 12:40:08 +0800

    @@

--- springboot-gradle/src/main/java/com/min/Application.java
+++ springboot-gradle/src/main/java/com/min/Application.java
@@ -11,7 +11,7 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		//SpringApplication.run(Application.class, args);
 		SpringApplication application = new SpringApplication(Application.class);
-		application.setLogStartupInfo(false);
+		//application.setLogStartupInfo(false);
 		//application.setBannerMode(Mode.OFF);
 		application.run(args);
 	}
--- springboot-gradle/src/main/java/com/min/web/WebConfig.java
+++ springboot-gradle/src/main/java/com/min/web/WebConfig.java
@@ -1,13 +1,20 @@
 package com.min.web;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module.Feature;
 
 @EnableWebMvc
 @Configuration
@@ -23,6 +30,17 @@ public class WebConfig implements WebMvcConfigurer {
 		        return new Date(Long.valueOf(source));
 			}
 		});
+	}
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {					
+		Hibernate5Module module = new Hibernate5Module();
+		module.configure(Feature.FORCE_LAZY_LOADING, true);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(module);
+		
+		converters.add(new MappingJackson2HttpMessageConverter(mapper));
 	}
 	
 }
