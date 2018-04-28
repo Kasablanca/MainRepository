84ea3755c6d13c5840ea5130e4bebd0323a473af
Kasablanca <programonkey@sina.com>
2018-04-28 11:18:12 +0800

    1!!

--- demo/src/test/java/cn/lm/test/TestCase.java
+++ demo/src/test/java/cn/lm/test/TestCase.java
@@ -59,8 +59,9 @@ import cn.lm.utils.ParamValidateUtil;
 public class TestCase {
 	
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello World!");
-		// hello world!
+		Integer i1 = 100, i2 = 100, i3 = 150, i4 = 150;
+		System.out.println(i1==i2);
+		System.out.println(i3 == i4);
 	}
 	
 	public static void getTreeTest() throws IOException {
